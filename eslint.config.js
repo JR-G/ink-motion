fcd2e7b7089bfc76e416e9f793e1babf57c0b4c3
James Glenn <47917431+JR-G@users.noreply.github.com>
2025-12-22 11:56:02 +0000

    Update ESLint configuration to v6
    - Update @antfu/eslint-config from v3.8.0 to v6.7.3
    - Add missing peer dependencies for React support
    - Disable react-refresh rule for demo files
    - Exclude markdown files from linting

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,9 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   react: true,
   typescript: true,
-  ignores: ['dist', 'coverage', 'node_modules'],
+  ignores: ['dist', 'coverage', 'node_modules', '**/*.md'],
+}, {
+  rules: {
+    'react-refresh/only-export-components': 'off',
+  },
 })
